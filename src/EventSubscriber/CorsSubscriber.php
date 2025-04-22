2b9d8124d934dbbc48678738660d08898f9b6727
Kerkar Kevin <kerkar.kevin@gmail.com>
2025-04-22 16:44:05 +0200

    Gestion CORS des requêtes entrantes

---
+++ src/EventSubscriber/CorsSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => ['onKernelRequest', 9999],
+            KernelEvents::RESPONSE => ['onKernelResponse', 9999],
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        if ($event->getRequest()->getMethod() === 'OPTIONS') {
+            $response = new Response();
+            $event->setResponse($response);
+        }
+    }
+
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Expose-Headers', 'Link');
+        $response->headers->set('Access-Control-Max-Age', '3600');
+    }
+}
